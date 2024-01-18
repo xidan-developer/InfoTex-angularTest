151c5375baa3b55b4ed5d9e753d6a7a04d1eebd5
xidan-developer <mezencev.daniil@icloud.com>
2024-01-18 18:45:09 +0700

    Final__commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SigninComponent} from "./signin/signin.component";
+import {SignupComponent} from "./signup/signup.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import { AuthGuard } from './shared/guards/auth.guard';
+import {CreateComponent} from "./create/create.component";
 
-const routes: Routes = [];
+const itemRoutes: Routes = [
+  { path: "create", component: CreateComponent},
+
+];
+
+const routes: Routes = [
+  { path: '', redirectTo: 'signin', pathMatch: 'full' },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard],children: itemRoutes },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,65 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { CreateComponent } from './create/create.component';
+import {AngularFireModule} from "@angular/fire/compat";
+import {SigninComponent} from "./signin/signin.component";
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { CardModule } from 'primeng/card';
+import { DashboardPipe } from './dashboard.pipe';
+import {FormsModule} from "@angular/forms";
+
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    SigninComponent,
+    SignupComponent,
+    DashboardComponent,
+    DashboardPipe
   ],
   imports: [
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    CheckboxModule,
+    RadioButtonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyAGya0gOwkmuuWTJpnzjjax05tzh15e6FY",
+      authDomain: "angular-infotex.firebaseapp.com",
+      projectId: "angular-infotex",
+      storageBucket: "angular-infotex.appspot.com",
+      messagingSenderId: "835993987829",
+      appId: "1:835993987829:web:e892cb00db524d5352f3bc"
+    })),
+    provideAuth(() => getAuth()),
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAGya0gOwkmuuWTJpnzjjax05tzh15e6FY",
+      authDomain: "angular-infotex.firebaseapp.com",
+      projectId: "angular-infotex",
+      storageBucket: "angular-infotex.appspot.com",
+      messagingSenderId: "835993987829",
+      appId: "1:835993987829:web:e892cb00db524d5352f3bc"
+    }),
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
