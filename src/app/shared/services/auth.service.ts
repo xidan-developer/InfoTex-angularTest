1345ef2dac6bd18439010da7c169341b5ee99718
xidan-developer <mezencev.daniil@icloud.com>
2024-01-19 08:45:46 +0700

    auth

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
         this.observeUserState()
       })
       .catch((error) => {
-        alert(error.message);
+        alert("Не правильно введенный пароль или логин");
       })
   }
 
