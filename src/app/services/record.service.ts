0c52790d87a867d7a57b9620b90bb12b93ae8ac3
xidan-developer <mezencev.daniil@icloud.com>
2024-01-19 08:37:21 +0700

    sorting reverse

--- src/app/services/record.service.ts
+++ src/app/services/record.service.ts
@@ -10,7 +10,9 @@ export class RecordService {
     { id: 2, text: `<p>2</p>`, image: 'img/image.png', timestamp: '18.01.2024 17:24' },
     { id: 3, text: `<p>3</p>`, image: 'img/image.png', timestamp: '18.01.2024 17:24' }
   ];
-
+  constructor() {
+    this.data = this.data.slice().reverse()
+  }
   getData(): Observable<any> {
     return of(this.data);
   }
@@ -19,7 +21,7 @@ export class RecordService {
     let date = new Date(times);
     let qweqwe =  ('0' + date.getDate()).slice(-2) + '.' + ('0' + (date.getMonth() + 1)).slice(-2) + '.' + date.getFullYear() + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
     const index = this.data.length
-    this.data.push({ id: index + 1, text: `${record}`, image: record.description, timestamp: qweqwe });
+    this.data.unshift({ id: index + 1, text: `${record}`, image: record.description, timestamp: qweqwe });
     console.log(this.data)
     return of(this.data);
 
